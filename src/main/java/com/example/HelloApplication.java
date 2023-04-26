7d3a745f4aabb8a6daad1158b78cb858f4a16eec
StevenSchBrightstraining <steven.schweikhard@brightstraining.com>
2023-04-26 18:19:53 +0200

    Probecode - Flappy Bird.
    Problem: Code zeigt keine Fehler an, erzeugt beim Ausführen allerdings einen Fehler.

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -7,11 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getRoot().requestFocus();
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
