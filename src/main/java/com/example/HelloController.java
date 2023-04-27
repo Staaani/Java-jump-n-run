fa7bfb0a07afb589825d29dd6d1169b3149cd33a
Staaani <stani.t91@gmail.com>
2023-04-27 10:32:42 +0200

    clean up code and add comments

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -5,7 +5,6 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -19,160 +18,133 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
 
-    AnimationTimer gameLoop;
-
+    AnimationTimer gameLoop; // Declare an instance of AnimationTimer called gameLoop
     @FXML
-    private AnchorPane plane;
+    private AnchorPane plane; // Reference to the main pane in our FXML file
     @FXML
-    private Rectangle bird;
-
+    private Rectangle bird; // Reference to the background image in our FXML file
     @FXML
-    private Rectangle platform1;
+    private ImageView background; // Reference to the background image in our FXML file
 
-    @FXML
-    private ImageView background;
-
-    double yDelta = 0.02;
+    double yDelta = 0.02; // Set a constant value for how fast we want the bird to fall down (y-axis)
     double time = 0;
-    int jumpHeight = 100;
 
-    int jumpWidth = 50;
-
-    // Load in our background image from resources folder
+    /* Load in our background image from resources folder and store it as an instance of Image.
+      We use getClass() method with getResourceAsStream() method because this is a JavaFX application,
+      not a standard Java application */
     Image backgroundImage = new Image(getClass().getResourceAsStream("/images/background.png"));
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        load();
+        load(); // Call load() method when program starts up
+        /* Set the ImageView's image property using setImage() method and pass it backgroundImage variable */
         background.setImage(backgroundImage);
         gameLoop = new AnimationTimer() {
+            /* Create an anonymous inner class that extends AnimationTimer and override its handle()
+               method which will be called every frame */
             @Override
             public void handle(long l) {
-                //update();
+                //update(); // Call update() method every frame
             }
         };
-        gameLoop.start();
+        gameLoop.start(); // Start the game loop
     }
 
     @FXML
     public void pressed(KeyEvent event) {
         if (event.getCode() == KeyCode.SPACE) {
-//            fly();
-            jump();
+            jump(); // Call jump() method when SPACE key is pressed
         } else if (event.getCode() == KeyCode.D) {
-            moveRight();
+            moveRight(); // Call moveRight() method when D key is pressed
         }else if (event.getCode() == KeyCode.A) {
-            moveLeft();
+            moveLeft(); // Call moveLeft() method when A key is pressed
         }
-//        else if (event.getCode() == KeyCode.S) {
-//            moveDown();
-//        }
+
     }
 
     private void moveRight() {
+        /* Move bird to the right by 20 pixels. This will be called in our keyPressed()
+          method whenever user presses 'D' */
         moveBird(20,0);
     }
     private void moveLeft() {
+        /* Move bird to the left by 20 pixels. This will be called in our keyPressed()
+         method whenever user presses 'A' */
         moveBird(-20,0);
     }
-//    private void moveDown() {
-//        moveBirdY(20);
-//    }
 
     private void jump() {
-        // If the bird is already in the air, don't allow another jump
+        /* Make the bird "jump" up by changing its y-coordinate.
+          We also schedule a task to make it fall back down after a certain amount of time */
         if (bird.getLayoutY() < 0) {
-            return;
+            return; // If bird has already jumped and hasn't fallen yet, don't allow another jump.
         }
 
-        // Move the bird up by 30 pixels
+        // Move the bird up by 10x ,30y pixels
         moveBird(10,-30);
 
-        // Schedule a task to move the bird back down after 0.3 seconds
+        /* Create an instance of Timeline with a duration of 0.3 seconds and add a KeyFrame that calls this lambda expression,
+              which moves the bird back down after jumping for some time */
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.15), event -> {
             moveBird(0,30);
             time = 0;
         }));
 
-        timeline.play();
+        timeline.play(); // Start the Timeline
     }
 
-//    private void fly() {
-//        if (bird.getLayoutY() + bird.getY() <= jumpHeight) {
-//            moveBirdY(-(bird.getLayoutY() + bird.getY()));
-//            time = 0;
-//            return;
-//        }
-//        moveBirdY(-jumpHeight);
-//        time = 0;
-//    }
-
     private void update() {
+        /* This method is called every frame by our game loop. It updates the bird's position and checks if it has collided with anything */
         time++;
-        moveBirdY(yDelta * time);
+        moveBird(0,yDelta * time);
 
         if (isBirdDead()) {
-            resetBird();
+            resetBird(); // If bird has hit the ground, reset its position to top of screen.
         }
     }
 
     private void load() {
-        System.out.println("Game starting");
+        System.out.println("Game starting"); // Print a message when program starts up
 
     }
 
-//    private void moveBirdY(double positionChange) {
-//        bird.setY(bird.getY() + positionChange);
-//    }
-
-    private void moveBirdY(double positionChange){
-        double oldY = bird.getY();
-        double newY = oldY + positionChange;
-
-        // Move bird up/down by given amount
-        bird.setY(newY);
-
-        // Check if we need to move our background too (if bird is near edge of screen)
-        double screenWidth = plane.getWidth();
-        if (bird.getBoundsInParent().getMaxX() >= 0.8 * screenWidth) {
-            background.setX(background.getX() - 0.5 * positionChange);
-        } else if (bird.getBoundsInParent().getMinX() <= 0.2 * screenWidth) {
-            background.setX(background.getX() - 0.5 * positionChange);
-        }
-    }
-
-    private void moveBirdX(double positionChange) {
-        bird.setX(bird.getX() + positionChange);
-    }
 
     private void moveBird(double xChange, double yChange) {
         double oldX = bird.getX();
         double newX = oldX + xChange;
 
-        // Move bird left/right by given amount
+        /* Calculate new X-coordinate for the bird after moving it left or right */
         bird.setX(newX);
 
         double oldY = bird.getY();
         double newY = oldY + yChange;
 
-        // Move bird up/down by given amount
+        /* Calculate new Y-coordinate for the bird after moving it up or down */
         bird.setY(newY);
 
-        // Check if we need to move our background too (if bird is near edge of screen)
+
         double screenWidth = plane.getWidth();
+        /* Check if we need to move our background too (if bird is near edge of screen) */
         if (bird.getBoundsInParent().getMaxX() >= 0.8 * screenWidth) {
             background.setX(background.getX() - 0.5 * xChange);
+            /* If the bird is on the right side of the screen, move the background image to left by half
+               as much as we moved the bird */
         } else if (bird.getBoundsInParent().getMinX() <= 0.2 * screenWidth) {
             background.setX(background.getX() - 0.5 * xChange);
+            /* If the bird is on the left side of the screen, move the background image to right by half
+                as much as we moved the bird */
         }
     }
 
     private boolean isBirdDead() {
+        /* This method checks whether or not our bird has collided with anything.
+          In this case, it just checks whether its y-coordinate exceeds that of our game window's height */
         double birdY = bird.getLayoutY() + bird.getY();
         return birdY >= plane.getHeight();
     }
 
     private void resetBird() {
+        // Reset position and time when called
         bird.setY(0);
         time = 0;
     }
